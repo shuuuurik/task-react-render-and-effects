b411a6c5781d5364d494d69a61d406e63be4c6bd
unknown <mayorov.shurik@mail.ru>
2022-05-27 21:40:34 +0300

    Рендеринг JSX

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,24 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useState, useEffect } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [lastMessage, setLastMessage] = useState(-1);
+
+    const messageHandler = (message: number) => setLastMessage(message);
+
+    useEffect(() => {
+        subscribe(props.sourceId, messageHandler);
+        return () => {
+            unsubscribe(props.sourceId, messageHandler);
+            setLastMessage(-1);
+        };
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            <h1>
+                {props.sourceId}: {lastMessage}
+            </h1>
+        </div>
+    );
 }
